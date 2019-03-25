68e90a12c915cb47b80a99892f27912e0fe41c52
eloylp <eloylpdev@gmail.com>
2019-03-25 19:47:43 +0100

    scaler mapper refactoring.

--- src/main/java/imagescaler/application/backend/ScalesMapper.java
+++ src/main/java/imagescaler/application/backend/ScalesMapper.java
@@ -10,7 +10,11 @@ import java.util.List;
 @Component
 class ScalesMapper {
 
+    // This may allow the comma separated format like "100x100,300x300,600x300"
+
     private static final String SCALE_PATTER = "^[0-9]+((?:[0-9]+)x(?:[0-9]+),?)+[0-9]$";
+    private static final String SCALE_SEPARATOR = ",";
+    private static final String SCALE_VALUES_SEPARATOR = "x";
 
     List<Scale> map(String rawScales) throws ImageScalerException {
 
@@ -18,12 +22,12 @@ class ScalesMapper {
             throw new ImageScalerException("Cannot parse scales argument :" + rawScales);
         }
 
-        String[] scales = rawScales.split(",");
+        String[] scales = rawScales.split(SCALE_SEPARATOR);
 
         List<Scale> results = new ArrayList<>();
 
         for (String scale : scales) {
-            String[] values = scale.split("x");
+            String[] values = scale.split(SCALE_VALUES_SEPARATOR);
             Scale scaleResult = new Scale(Integer.parseInt(values[0]), Integer.parseInt(values[1]));
             results.add(scaleResult);
         }
