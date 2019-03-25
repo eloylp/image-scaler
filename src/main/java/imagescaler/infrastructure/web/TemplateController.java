1584d6047a59cc6a23e892914c77b01a88c35bf7
eloylp <eloylpdev@gmail.com>
2019-03-25 23:42:22 +0100

    removed dead code.

--- src/main/java/imagescaler/infrastructure/web/TemplateController.java
+++ src/main/java/imagescaler/infrastructure/web/TemplateController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 class TemplateController {
@@ -14,14 +13,12 @@ class TemplateController {
 
     @GetMapping("/upload")
     public String upload(Model model) {
-        ModelAndView mav = new ModelAndView("upload");
         model.addAttribute("appName", appName);
         return "upload";
     }
 
     @GetMapping("/")
     public String index(Model model) {
-        ModelAndView mav = new ModelAndView("index");
         model.addAttribute("appName", appName);
         return "index";
     }
