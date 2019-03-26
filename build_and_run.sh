4965d056bb7eeae1b4d256c3e67ef0c85289411c
eloylp <eloylpdev@gmail.com>
2019-03-26 20:32:55 +0100

    updated host and port env vars related to docker compose and init script.

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 
-if [ -z "$1" ]
+if [ -z "$1" ] || [ -z "$2" ]
 then
-    echo "We need the app name as first parameter"
+    echo "We need the app name as first parameter. As second, your host port."
     exit 1
 fi
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     echo "Illegal number of parameters"
     exit 1
 fi
@@ -15,5 +15,5 @@ fi
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd .
 cd $HERE
-mvn package && APP_NAME=$1 docker-compose up --build
+mvn package && APP_NAME=$1 HTTP_HOST_PORT=$2 docker-compose up --build
 popd
